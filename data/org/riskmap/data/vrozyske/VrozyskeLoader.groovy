7086cd00a462c766152d2c90573fe336eca7c6da
Maxim Galushka <mgalushka@gmail.com>
2013-11-13 18:14:11 +0200

    last version

--- data/org/riskmap/data/vrozyske/VrozyskeLoader.groovy
+++ data/org/riskmap/data/vrozyske/VrozyskeLoader.groovy
@@ -7,6 +7,7 @@ import org.apache.http.HttpHost
 import org.htmlparser.Parser
 import org.htmlparser.filters.AndFilter
 import org.htmlparser.filters.HasAttributeFilter
+import org.htmlparser.filters.StringFilter
 import org.htmlparser.filters.TagNameFilter
 import org.htmlparser.tags.LinkTag
 
@@ -25,7 +26,6 @@ Logger.rootLogger.level = Level.WARN
 
 def parse_number_of_pages = { String html ->
     try {
-        List<String> result = new ArrayList<String>();
         def parser = new Parser(html).parse(
                 new AndFilter(
                         new TagNameFilter("a"),
@@ -42,6 +42,30 @@ def parse_number_of_pages = { String html ->
         return 1;
     }
 }
+
+def parse_person_url = { String html ->
+    try {
+        def parser = new Parser(html).parse(
+                new AndFilter(
+                        new TagNameFilter("div"),
+                        new HasAttributeFilter("class", "catalog_list_item")
+                )
+        );
+
+        org.htmlparser.util.NodeList nodes = parser.extractAllNodesThatMatch(
+                new AndFilter(
+                        new TagNameFilter("a"),
+                        new StringFilter("class"))
+        );
+        if (nodes.length > 2) {
+            def link = (LinkTag) nodes[nodes.length - 3]
+            return Integer.parseInt(link.getText())
+        }
+    } catch (Exception e) {
+        return 1;
+    }
+}
+
 
 def VROZYSKE_HOSTNAME = "vrozyske.org"
 def HOST = new HttpHost(VROZYSKE_HOSTNAME)
@@ -60,7 +84,9 @@ def people = []
         println "Processing letter ${L}";
 
         def hs = new HttpHelper<String>(httpClient);
-        def h = new HttpHelper<Person>(httpClient);
+
+        // parsed list of people on single page
+        def hp = new HttpHelper<List<Person>>(httpClient);
 
         // find all a class="pagebar_page" - number of pages
         def letterEncoded = URLEncoder.encode(L.toString(), "windows-1251")
@@ -74,15 +100,15 @@ def people = []
                 }, "windows-1251");
 
         // for all pages
-        for (def page = 1; page <= 0; page++) {
+        for (def page = 1; page <= N; page++) {
             def url = "/catalog/${year}-${page}/find-first/${letterEncoded}";
             println("Processing URL: ${url}");
 
-            def person = h.get(url, HOST,
-                    new HttpCallbackHandler<Person>() {
+            def person = hp.get(url, HOST,
+                    new HttpCallbackHandler<List<Person>>() {
                         @Override
-                        public Person process(String content) throws Exception {
-                            return null;
+                        public List<Person> process(String content) throws Exception {
+                            return Collections.emptyList();
                         }
                     }, "windows-1251");
             people << person;
